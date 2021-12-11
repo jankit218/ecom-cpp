ce48712a1f04e5a68123652218af34b18c84bebc
Shrikant Thombre <shri1900@gmail.com>
2021-12-11 00:45:24 +0000

    db and config changes for deployment

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,8 +2,6 @@
 #Run the application
 
 sudo chmod -R 775 /home/ubuntu/cpp/myproj
-#sudo chown ec2-user:ec2-user /home/ec2-user/cpp
-#sudo chown ec2-user:ec2-user /home/ec2-user/cpp/*
 
 cd /home/ubuntu/cpp
 python3 -m venv venv
@@ -15,5 +13,6 @@ pip install -r requirements.txt
 sudo chown ubuntu:ubuntu *
 
 echo "Running the application"
-python manage.py runserver 0.0.0.0:80 >> /dev/null 2>&1 &
+cd /home/ubuntu/cpp/myproj
+python3 manage.py runserver 0.0.0.0:80 >> /dev/null 2>&1 &
 
